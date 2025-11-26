9939c385662309c1d4f6c01a3687a99c77e6535b
Shao-yan (Matt) Ji <e@mail.shaoyanji@is-a.dev>
2025-11-26 20:48:09 +0100

    made big changes to custom plugins

--- config/plugins/custom/default.nix
+++ config/plugins/custom/default.nix
@@ -3,62 +3,81 @@
   pkgs,
   ...
 }: {
-  plugins.supermaven = {
-    enable = true;
-    settings = {
-      keymaps = {
-        accept_suggestion = "<Tab>";
-        clear_suggestions = "<C-]>";
-        accept_word = "<C-j>";
-      };
-      ignore_filetypes = ["cpp"];
-      color = {
-        suggestion_color = "#ffffff";
-        cterm = 244;
-      };
-      log_level = "info";
-      disable_inline_completion = false;
-      disable_keymaps = false;
-      condition = lib.nixvim.mkRaw ''
-        function()
-          return false
-        end
-      '';
+  plugins = {
+    typst-vim.enable = true;
+    typst-preview.enable = true;
+    cmp-tabnine.enable = true;
+    cloak.enable = true;
+    cloak.settings = {
+      cloak_character = "*";
+      enabled = true;
+      highlight_group = "Comment";
+      patterns = [
+        {
+          cloak_pattern = "=.+";
+          file_pattern = [
+            ".env*"
+          ];
+        }
+      ];
     };
-  };
-  plugins.obsidian.enable = true;
-  plugins.obsidian.settings = {
-    legacy_commands = false;
-    completion = {
-      min_chars = 2;
+    nix-develop.enable = true;
+    otter.enable = true;
+    supermaven = {
+      enable = true;
+      settings = {
+        keymaps = {
+          accept_suggestion = "<C-CR>";
+          clear_suggestions = "<C-]>";
+          accept_word = "<C-j>";
+        };
+        ignore_filetypes = ["cpp"];
+        color = {
+          suggestion_color = "#ffffff";
+          cterm = 244;
+        };
+        log_level = "info";
+        disable_inline_completion = false;
+        disable_keymaps = false;
+        condition = lib.nixvim.mkRaw ''
+          function()
+            return false
+          end
+        '';
+      };
     };
-    new_notes_location = "current_dir";
-    workspaces = [
-      {
-        name = "work";
-        path = "~/vaults/work";
-      }
-      {
-        name = "startup";
-        path = "~/vaults/personal";
-      }
-    ];
-  };
-  extraPlugins = with pkgs.vimPlugins; [
-  ];
-  plugins.lsp = {
-    servers = {
-      gopls.enable = true;
-      nixd = {
-        enable = true;
-        settings = {
-          nixd = {
-            formatting = {
-              command = "${pkgs.alejandra}/bin/alejandra";
-            };
-            options = {
-              nixpkgs = {
-                expr = "import <nixpkgs> {}";
+    obsidian.enable = true;
+    obsidian.settings = {
+      legacy_commands = false;
+      completion = {
+        min_chars = 2;
+      };
+      new_notes_location = "current_dir";
+      workspaces = [
+        {
+          name = "work";
+          path = "~/vaults/work";
+        }
+        {
+          name = "startup";
+          path = "~/vaults/personal";
+        }
+      ];
+    };
+    lsp = {
+      servers = {
+        gopls.enable = true;
+        nixd = {
+          enable = true;
+          settings = {
+            nixd = {
+              formatting = {
+                command = "${pkgs.alejandra}/bin/alejandra";
+              };
+              options = {
+                nixpkgs = {
+                  expr = "import <nixpkgs> {}";
+                };
               };
             };
           };
@@ -66,4 +85,6 @@
       };
     };
   };
+  extraPlugins = with pkgs.vimPlugins; [
+  ];
 }
