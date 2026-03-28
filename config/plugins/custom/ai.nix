9803732b38fd2829224054767ab7e3f2d22017ea
matt <github@mail.shaoyanji.is-a.dev>
2026-03-28 01:49:23 +0000

    adding final

--- config/plugins/custom/ai.nix
+++ config/plugins/custom/ai.nix
@@ -1,5 +1,3 @@
 {
-  # Supermaven currently aborts startup when its external binary metadata is blank
-  # or otherwise unavailable. Keep AI config non-fatal by disabling it entirely
-  # until it can be reintroduced with a safe initialization path.
+  
 }
--- config/plugins/kickstart/gitsigns.nix
+++ config/plugins/kickstart/gitsigns.nix
@@ -5,6 +5,11 @@
   plugins.gitsigns = {
     enable = true;
     settings = {
+      current_line_blame = true;
+      current_line_blame_opts = {
+        delay = 500;
+        ignore_whitespace = false;
+      };
       signs = {
         add.text = "+";
         change.text = "~";
